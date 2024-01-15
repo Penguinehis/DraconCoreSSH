c079b6f1676a9be68bee5144714e548ee104ef94
Penguinehis <54519344+Penguinehis@users.noreply.github.com>
2024-01-14 22:29:21 -0300

    Fix 21

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ apt update
 apt upgrade -y
 apt install php-cli php-curl php-sqlite3 git -y
 cake="$(uname -m)"
-if [ "$cake" == "x86_64"]; then
+if [ "$cake" == "x86_64" ]; then
 curl -O https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
 tar -xvzf ioncube_loaders_lin_x86-64.tar.gz
 rm ioncube_loaders_lin_x86-64.tar.gz
